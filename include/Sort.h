1cd8c9a5a1c6f82450ab81fe794be081ef628072
Michael.Yang <Michael.Yang@imgtec.com>
2014-09-01 23:10:13 +0800

    1.4 anagrams

--- include/Sort.h
+++ include/Sort.h
@@ -3,5 +3,5 @@ class Sort {
 public:
 	Sort();
 	virtual ~Sort();
-	virtual void doSort(int* arr, int size, int left, int right) = 0;
+	virtual void doSort(int arr[], int size, int left, int right) = 0;
 };
